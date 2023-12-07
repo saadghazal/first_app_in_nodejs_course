b033b73ef44061c5a673f29e8bb5bb0a40ebb7b6
saadghazal <saad.m.ghazal@icloud.com>
2023-12-07 15:25:23 +0300

    first version

---
+++ first-app.js
@@ -0,0 +1,6 @@
+const fs = require("fs");// Stands for File System it's offered by Node.js
+
+
+// the first argument is the path of the new file
+// the second argument is the content of the new file
+fs.writeFileSync('samer.txt','Samer is a Tawjihi Student from');

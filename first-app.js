6449c84e1033f239f0abbfe25ba1dda46acb2748
saadghazal <saad.m.ghazal@icloud.com>
2023-12-07 20:43:27 +0300

    Promise

--- first-app.js
+++ first-app.js
@@ -1,82 +1,20 @@
-// Destructuring in Objects
-
-const person = {
-    name: "Max",
-    age: 29,
-    greet(){
-        console.log("Hi, I'm "+this.name)
-    }
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Done")
+        }, 1500);
+    })
+    return promise
 }
 
-
-const printName = ({name})=>{
-  console.log(name)
-}
-
-
-printName(person);
-
-const {name,age} = person
-
-console.log(name,age)
-
-// Destructuring in Arrays
-const hobbies = ['Cars','Football'];
-const [hobby1] = hobbies
-console.log(hobby1)
-
-
-
-// const fs = require("fs");  Stands for File System it's offered by Node.js
-
-
-// the first argument is the path of the new file
-// the second argument is the content of the new file
-// fs.writeFileSync('samer.txt','Samer is a Tawjihi Student from');
-/*
-
-1- the this keyword in here referese to the whole node js file so it will output Hi, I'm undefined
-const person = {
-    name: "Max",
-    age: 29,
-    greet: ()=>{
-        console.log("Hi, I'm "+this.name)
-    }
-}*/
-
-/*
-2- the this keyword in here referse to the person object itself so the output will be Hi, I'm Max
-const person = {
-    name: "Max",
-    age: 29,
-    greet: function (){
-        console.log("Hi, I'm "+this.name)
-    }
-}*/
-
-/*
-// 3- the this keyword in here referse to the person object itself so the output will be Hi, I'm Max
-// The Spread Operator
-const person = {
-    name: "Max",
-    age: 29,
-    greet(){
-        console.log("Hi, I'm "+this.name)
-    }
-}
-
-const copiedPerson = {...person};
-
-console.log(copiedPerson)
-const hobbies = ['Cars','Football'];
-const copiedArray = [...hobbies];
-console.log(copiedArray)
-
-//Rest Operator
-
-const toArray = (...args)=>{
-return args
-}
-
-console.log(toArray(3,6,10,0.394683094684309,))
-*/
+// this function by nodejs itself it will execute the function after period of time 
+//the time is set in milliseconds
+setTimeout(() => {
+    console.log("Timer is done")
+    fetchData().then(text => {
+        console.log(text)
+        return fetchData()
+    },).then(text2 => {
+        console.log(text2)
+    },)
+}, 2000);

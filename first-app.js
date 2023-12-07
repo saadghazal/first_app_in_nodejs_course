d29764ef745984b529b6ad23bbcfd3a06f6a47a3
saadghazal <saad.m.ghazal@icloud.com>
2023-12-07 20:16:34 +0300

    Destructuring

--- first-app.js
+++ first-app.js
@@ -1,9 +1,38 @@
-const fs = require("fs");// Stands for File System it's offered by Node.js
+// Destructuring in Objects
+
+const person = {
+    name: "Max",
+    age: 29,
+    greet(){
+        console.log("Hi, I'm "+this.name)
+    }
+}
+
+
+const printName = ({name})=>{
+  console.log(name)
+}
+
+
+printName(person);
+
+const {name,age} = person
+
+console.log(name,age)
+
+// Destructuring in Arrays
+const hobbies = ['Cars','Football'];
+const [hobby1] = hobbies
+console.log(hobby1)
+
+
+
+// const fs = require("fs");  Stands for File System it's offered by Node.js
 
 
 // the first argument is the path of the new file
 // the second argument is the content of the new file
-fs.writeFileSync('samer.txt','Samer is a Tawjihi Student from');
+// fs.writeFileSync('samer.txt','Samer is a Tawjihi Student from');
 /*
 
 1- the this keyword in here referese to the whole node js file so it will output Hi, I'm undefined
@@ -25,7 +54,7 @@ const person = {
     }
 }*/
 
-
+/*
 // 3- the this keyword in here referse to the person object itself so the output will be Hi, I'm Max
 // The Spread Operator
 const person = {
@@ -50,3 +79,4 @@ return args
 }
 
 console.log(toArray(3,6,10,0.394683094684309,))
+*/
